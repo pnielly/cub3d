e57c4d59bd661267092dc572ca1029023450881d
Padoue NIELLY <pnielly@e2r7p9.42.fr>
2020-02-04 23:01:28 +0100

    current

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnielly <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/23 14:49:28 by pnielly           #+#    #+#             */
+/*   Updated: 2019/10/24 11:49:21 by pnielly          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	tmp = *lst;
+	if (tmp->next)
+	{
+		tmp = tmp->next;
+		ft_lstdelone(tmp, del);
+	}
+	free(tmp);
+	*lst = NULL;
+}
