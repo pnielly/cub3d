15f63d5f04c6a1519423bf521da7f2248926c319
Padoue NIELLY <pnielly@e3r3p1.42.fr>
2020-02-25 17:42:25 +0100

    tout

---
+++ srcs/color.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_color.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnielly <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/18 16:07:58 by pnielly           #+#    #+#             */
+/*   Updated: 2020/02/24 18:22:34 by pnielly          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+/*
+ ** ft_color only concerns ceiling and floor
+*/
+
+unsigned int	ft_color(void *mlx_ptr, t_color *zone)
+{
+	static int	color;
+
+	color = 0;
+	color = color << 8;
+	color = (color << 8) + zone->r;
+	color = (color << 8) + zone->g;
+	color = (color << 8) + zone->b;
+	return (mlx_get_color_value(mlx_ptr, color));
+}
+
+/*
+ ** ft_tex_color concerns wall textures
+ ** (gives the color of each textured pixel)
+*/
+
+unsigned int	ft_tex_color(t_img *tex, int x, int y)
+{
+	return (tex->img_data_addr[y * WALL_HEIGHT + x]);
+}
+
+unsigned int	ft_color_mix(t_stock *stock,
+		unsigned int color, double dist, int y)
+{
+	unsigned int	r;
+	unsigned int	g;
+	unsigned int	v;
+
+	if (dist > 2 && y > stock->wall_apex && y < stock->wall_bottom)
+	{
+		r = color / (16 * 16 * 16 * 16);
+		g = (color / (16 * 16)) % (16 * 16);
+		v = color % (16 * 16);
+		r = FOG / (16 * 16 * 16 * 16) * (1 - 1 / dist * 0.2) + r / (dist / 2);
+		g = ((FOG / (16 * 16)) % (16 * 16)) * (1 - 1 / dist * 0.2)
+			+ g / (dist / 2);
+		v = FOG % (16 * 16) * (1 - 1 / dist * 0.2) + v / (dist / 2);
+		color = r * (16 * 16 * 16 * 16) + g * (16 * 16) + v;
+	}
+	return (color);
+}
---
+++ srcs/init_struct.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init_struct.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnielly <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/19 21:30:30 by pnielly           #+#    #+#             */
+/*   Updated: 2020/02/25 17:34:16 by pnielly          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	ft_init_struct_ray(t_ray *ray)
+{
+	ray->res_x = 0;
+	ray->res_y = 0;
+	ray->delta_x = 0;
+	ray->delta_y = 0;
+	ray->step_x = 0;
+	ray->step_y = 0;
+	return (0);
+}
+
+int	ft_init_struct_move(t_move *move)
+{
+	move->forward = 0;
+	move->backward = 0;
+	move->right = 0;
+	move->left = 0;
+	move->turn_right = 0;
+	move->turn_left = 0;
+	return (0);
+}
+
+int	ft_init_struct_stock(t_stock *stock)
+{
+	stock->wall_height = WALL_HEIGHT;
+	stock->cam_height = stock->wall_height / 2;
+	stock->cam_fov = CAM_FOV;
+	stock->escape = 0;
+	stock->shade = 1;
+	stock->speed = SPEED;
+	stock->lost = 0;
+	stock->won = 0;
+	if (!(stock->ray = malloc(sizeof(t_ray))))
+		return (ft_putstr("MALLOC FAILED (RAY)\n"));
+	ft_init_struct_ray(stock->ray);
+	if (!(stock->move = malloc(sizeof(t_move))))
+		return (ft_putstr("MALLOC FAILED (MOVE)\n"));
+	ft_init_struct_move(stock->move);
+	if (!(stock->player = malloc(sizeof(t_player))))
+		return (ft_putstr("MALLOC FAILED (PLAYER)\n"));
+	stock->nb_sprites = 0;
+	return (0);
+}
